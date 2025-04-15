bb5469ef691e7e6080102e725dc3b2c20e43b8b8
JackReevesEyre-NOAA <jack.reeveseyre@noaa.gov>
2025-04-15 10:36:06 -0400

    Adds features to reactive dashboard.

--- stofs-event-dashboard/dashboard_reactive.py
+++ stofs-event-dashboard/dashboard_reactive.py
@@ -145,7 +145,7 @@ class UI:
 def get_static_map(weather_event):
     static_map = pn.pane.image.PNG(
         object=DATA_DIR / f"{weather_event}/static_map.png", 
-        height=400
+        height=300
     )
     return static_map
 
@@ -179,6 +179,129 @@ def plot_ts(weather_event, models, station, percentile, window):
     timeseries += [hv.HLine(obs_threshold).opts(color="grey", line_dash="dashed", line_width=1)]
     timeseries += [obs_ext.hvplot.scatter(label="obs extreme")]
     return hv.Overlay(timeseries).opts(show_grid=True, active_tools=["box_zoom"], min_height=300, ylabel="sea elevation")
+
+@pn.depends(UI.weather_events,
+            UI.models,
+            UI.station, 
+            UI.quantile, 
+            UI.window)
+def plot_table_comparison(weather_event, models, station, percentile, window):
+    quantile: float = T.cast(float, percentile) / 100
+    obs = load_data(DATA_DIR / f"{weather_event}/obs/{station}.parquet")
+    start, end = obs.index.min(), obs.index.max()
+    sims = {model: load_data(DATA_DIR / f"{weather_event}/models/{model}/{station}.parquet")[start:end] for model in models}
+    stats = {
+        model: seastats.get_stats(sim, obs, quantile=quantile, cluster=window, round=3)
+        for model, sim in sims.items()
+    }
+    logger.info("stats:\n%r", stats)
+    return pd.DataFrame(stats).T
+
+
+@pn.depends(UI.weather_events,
+            UI.models,
+            UI.station)
+def plot_scatter(weather_event, models, station):
+    # data
+    obs = load_data(DATA_DIR / f"{weather_event}/obs/{station}.parquet")
+    start, end = obs.index.min(), obs.index.max()
+    sims = {model: load_data(DATA_DIR / f"{weather_event}/models/{model}/{station}.parquet")[start:end] for model in models}
+    # plots
+    plots = [hv.Slope(1, 0, label="45°").opts(color="grey", show_grid=True)]
+    pp_plots = []
+    for i, (model, sim) in enumerate(sims.items()):
+        color = cc.glasbey_dark[i]
+        sim_, obs_ = seastats.align_ts(sim, obs)
+        # slope, intercept = seastats.get_slope_intercept(sim_, obs_)
+        scatter = hv.Scatter((sim_, obs_), label=model)
+        plots.append(scatter.opts(color=color, muted=True))
+        plots.append(
+            hv.Slope.from_scatter(scatter).opts(
+                color=color,
+                line_dash="dashed",
+            )
+        )
+        pc1, pc2 = seastats.get_percentiles(sim_, obs_, higher_tail=True)
+        pp_scatter = hv.Scatter((pc1, pc2), label=f"Percentiles {model}")
+        pp_plots += [
+            pp_scatter.opts(color=color, line_color="k", line_width=2, size=8, tools=["hover"]),
+            hv.Curve((pc1, pc2)).opts(color=cc.glasbey_dark[-i]),
+        ]
+    overlay = hv.Overlay(plots + pp_plots)
+    overlay = overlay.opts(
+        min_height=400,
+        max_height=500,
+        show_grid=True,
+        legend_position="right",
+        title="Title",
+        xlabel="observed",
+        ylabel="model",
+    )
+    return overlay
+
+
+@pn.depends(UI.weather_events,
+            UI.models,
+            UI.station, 
+            UI.quantile, 
+            UI.window)
+def plot_extremes_table(weather_event, models, station, percentile, window):
+    quantile = percentile / 100
+    # data
+    obs = load_data(DATA_DIR / f"{weather_event}/obs/{station}.parquet")
+    start, end = obs.index.min(), obs.index.max()
+    sims = {model: load_data(DATA_DIR / f"{weather_event}/models/{model}/{station}.parquet")[start:end] for model in models}
+    model_dfs = []
+    for i, (model, sim) in enumerate(sims.items()):
+        # color = cc.glasbey_dark[i]
+        ext_df = match_extremes(sim=sim, obs=obs, quantile=quantile, cluster=window)
+        logger.info("\n%r", ext_df)
+        model_dfs += [ext_df[["model"]].rename(columns={"model": model})]
+    if len(sims) > 0:
+        model_dfs.insert(0, ext_df[["observed"]].rename(columns={"observed": "obs"}))
+        df = pd.concat(model_dfs, axis=1)
+        table = pn.widgets.Tabulator(df.round(3), max_height=400)
+    else:
+        table = pn.widgets.Tabulator(pd.DataFrame())
+    return table
+
+
+def match_extremes(
+    sim: pd.Series[float],
+    obs: pd.Series[float],
+    quantile: float,
+    cluster: int
+) -> pd.DataFrame:
+    # get observed extremes
+    ext = pyextremes.get_extremes(obs, "POT", threshold=obs.quantile(quantile), r=f"{cluster}h")
+    ext_values_dict: dict[str, T.Any] = {}
+    ext_values_dict["observed"] = ext.values
+    ext_values_dict["time observed"] = ext.index.values
+    #
+    max_in_window = []
+    tmax_in_window = []
+    # match simulated values with observed events
+    for it, itime in enumerate(ext.index):
+        snippet = sim[itime - pd.Timedelta(hours=cluster / 2) : itime + pd.Timedelta(hours=cluster / 2)]
+        try:
+            tmax_in_window.append(snippet.index[int(snippet.argmax())])
+            max_in_window.append(snippet.max())
+        except Exception:
+            tmax_in_window.append(itime)
+            max_in_window.append(np.nan)
+    ext_values_dict["model"] = max_in_window
+    ext_values_dict["time model"] = tmax_in_window
+    #
+    df = pd.DataFrame(ext_values_dict)
+    df = df.dropna(subset="model")
+    df = df.sort_values("observed", ascending=False)
+    df["diff"] = df["model"] - df["observed"]
+    df["error"] = abs(df["diff"])
+    df["error_norm"] = abs(df["diff"] / df["observed"])
+    df["tdiff"] = df["time model"] - df["time observed"]
+    df["tdiff"] = df["tdiff"].apply(lambda x: x.total_seconds() / 3600)
+    df = df.set_index("time observed")
+    return df
     
 
 page = pn.template.MaterialTemplate(
@@ -193,10 +316,17 @@ page = pn.template.MaterialTemplate(
         UI.quantile,
         UI.window,
     ],
-    sidebar_width=430,
+    sidebar_width=350,
     main=pn.Column(
         get_static_map,
-        plot_ts,
+        pn.Tabs(
+            ('Time series', plot_ts),
+            ('Statistics', plot_table_comparison),
+            ('Scatter', plot_scatter), 
+            ('Extremes', plot_extremes_table),
+            tabs_location='left',
+            dynamic=True
+        )
     ),
 )
 page.servable()
