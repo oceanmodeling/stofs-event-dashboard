f4d2c404e96a667f476bfc04cbe3e41a282e6527
JackReevesEyre-NOAA <jack.reeveseyre@noaa.gov>
2025-05-28 14:48:06 -0400

    Updates processing to avoid re-processing data that's already been saved.

--- stofs-event-dashboard/models.py
+++ stofs-event-dashboard/models.py
@@ -3,9 +3,13 @@
 
 
 import datetime
+import pandas as pd
+import numpy as np
 from typing import List
 import logging
 import seanode
+from space_time_bounds import eventSpaceTimeBounds
+import write_output
 
 
 logger = logging.getLogger(__name__)
@@ -48,19 +52,19 @@ def get_forecast_init_times(
     return result
 
 
-def get_forcing_model(model: str) -> str:
-    """Returns the atmosphere model used to force a given surge model."""
+def get_model_forcing(model: str) -> str:
+    """Returns the model name used to download data."""
     if model in ['stofs_2d_glo']:
-        forcing_model = 'GFS'
+        forcing_name = 'GFS'
     elif model in ['stofs_3d_atl']:
-        forcing_model = 'HRRR'
+        forcing_name = 'HRRR'
     else:
         raise ValueError(f'Model {model} not available in stofs-event-dashboard.')
-    return forcing_model
+    return forcing_name
 
 
 def get_forcing_geometry(model: str) -> str:
-    """Returns the file geometry for data from a given model's forcing."""
+    """Returns the file geometry for data from a given model/forcing."""
     if model in ['stofs_2d_glo']:
         geom = 'grid'
     elif model in ['stofs_3d_atl']:
@@ -68,4 +72,220 @@ def get_forcing_geometry(model: str) -> str:
     else:
         raise ValueError(f'Model {model} not available in stofs-event-dashboard.')
     return geom
+
+
+def get_dir_var_name(var_name: str) -> str:
+    """Gets version of variable name used in data directory structure."""
+    if var_name in ['cwl', 'water_level']:
+        dir_var = 'cwl'
+    elif var_name in ['pressure', 'air_pressure']:
+        dir_var = 'pressure'
+    elif var_name in ['wind']:
+        dir_var = 'wind'
+    else:
+        raise ValueError(f'var_name {var_name} not recognized.')
+    return dir_var
+
+
+def check_run_query(
+    forecast_type,
+    out_dir,
+    start_datetime,
+    end_datetime
+):
+    """
+    """
+    run_query = False
+    append_data = False
+    query_start_datetime = None
+
+    if forecast_type == 'nowcast':
+        # Check if the obs files exist and need to be updated.
+        existing_files = list(out_dir.glob('*.parquet'))
+        if len(existing_files) == 0:
+            # If no files exist, we want to run the query and save files. 
+            run_query = True
+            query_start_datetime = start_datetime
+        else:
+            # If files exist, we only want to run a query if the end time
+            # of the files is less than the end time of the event. Then we
+            # want to append new data, instead of overwriting the file.
+            # TODO: Check if there is a faster way to do this than reading all the files.
+            earliest_end_datetime = min([
+                pd.read_parquet(fn).reset_index()['time'].max() 
+                for fn in existing_files
+            ]).to_pydatetime()
+            if earliest_end_datetime < end_datetime:
+                run_query = True
+                append_data = True
+                query_start_datetime = max(
+                    earliest_end_datetime + datetime.timedelta(minutes=1),
+                    start_datetime
+                )
+                
+    elif forecast_type == 'forecast':
+        # For forecast data, we just check if the output directory exists and 
+        # if there are files in it.
+        existing_files = list(out_dir.glob('*.parquet'))
+        if len(existing_files) == 0:
+            # If no files exist, we want to run the query and save files. 
+            run_query = True
+            query_start_datetime = start_datetime
+
+    else:
+        raise ValueError(f'forecast_type {forecast_type} not recognized in check_run_query.')
+    #
+    return run_query, append_data, query_start_datetime
+
+
+def save_model(
+    waterlevel_stations: pd.DataFrame, 
+    met_stations: pd.DataFrame, 
+    stb: eventSpaceTimeBounds, 
+    model_config_dict: dict,
+    plot_types_config:dict,
+    output_config: dict,
+    start_time: datetime.datetime,
+    end_time: datetime.datetime | None
+) -> None:
+    """
+    """
+    # Parse options.
+    if len(model_config_dict.keys()) > 1:
+        raise ValueError('Models should be passed to save_model(...) one at a time.')
+    else:
+        model_name = list(model_config_dict.keys())[0]
+        model_config = model_config_dict[model_name]
+    if end_time is not None:
+        forecast_type = 'nowcast'
+        forecast_type_dir = 'nowcast'
+    else:
+        forecast_type = 'forecast'
+        forecast_type_dir = f'forecast_{start_time.strftime('%Y%m%dT%H%M')}'
+    forcing_model = get_model_forcing(model_name)
+    forcing_geom = get_forcing_geometry(model_name)
+    if output_config['output_datum'] == 'NAVD':
+        output_datum_model = 'NAVD88'
+    else:
+        output_datum_model = output_config['output_datum']
+    data_dir = write_output.get_output_dir(output_config, stb)
+
+    # Download and save water level data.
+    if plot_types_config['water_level']:
+        out_dir = data_dir / f'cwl/{forecast_type_dir}/{model_name}'
+        run_query, append_data, query_start_datetime = check_run_query(
+            forecast_type, out_dir, start_time, end_time
+        )
+        if run_query:
+            model_data = seanode.api.get_surge_model_at_stations(
+                model_name.upper(),
+                model_config['water_level_var_list'],
+                waterlevel_stations.nos_id,
+                query_start_datetime,
+                end_time,
+                forecast_type,
+                'points',
+                output_datum_model,
+                'AWS'
+            )
+            write_output.df_sealens_parquet(
+                model_data,
+                out_dir,
+                column_names=model_config['water_level_var_list'],
+                append=append_data
+            )
+        else:
+            logger.info(f'Not fetching any {model_name.upper()} CWL data, probably because it already exists for given date range ({start_time} -- {end_time})')
+            
+    # Download pressure and/or wind data.
+    if plot_types_config['pressure'] & plot_types_config['wind']:
+        # For simplicity we just test whether the wind data exists.
+        out_dir = data_dir / f'wind/{forecast_type_dir}/{model_name}'
+        run_query, append_data, query_start_datetime = check_run_query(
+            forecast_type, out_dir, start_time, end_time
+        )
+        if run_query:
+            model_data = seanode.api.get_surge_model_at_stations(
+                forcing_model,
+                model_config['pressure_var_list'] + 
+                model_config['wind_var_list'],
+                met_stations,
+                query_start_datetime,
+                end_time,
+                forecast_type,
+                forcing_geom,
+                None,
+                'AWS'
+            )
+        else:
+            logger.info(f'Not fetching any {model_name.upper()} wind+pressure data, probably because it already exists for given date range ({start_time} -- {end_time})')
+    elif plot_types_config['pressure']:
+        out_dir = data_dir / f'pressure/{forecast_type_dir}/{model_name}'
+        run_query, append_data, query_start_datetime = check_run_query(
+            forecast_type, out_dir, start_time, end_time
+        )
+        if run_query:
+            model_data = seanode.api.get_surge_model_at_stations(
+                forcing_model,
+                model_config['pressure_var_list'],
+                met_stations,
+                query_start_datetime,
+                end_time,
+                forecast_type,
+                forcing_geom,
+                None,
+                'AWS'
+            )
+        else:
+            logger.info(f'Not fetching any {model_name.upper()} pressure data, probably because it already exists for given date range ({start_time} -- {end_time})')
+    elif plot_types_config['wind']:
+        out_dir = data_dir / f'wind/{forecast_type_dir}/{model_name}'
+        run_query, append_data, query_start_datetime = check_run_query(
+            forecast_type, out_dir, start_time, end_time
+        )
+        if run_query:
+            model_data = seanode.api.get_surge_model_at_stations(
+                forcing_model,
+                model_config['wind_var_list'],
+                met_stations,
+                query_start_datetime,
+                end_time,
+                forecast_type,
+                forcing_geom,
+                None,
+                'AWS'
+            )
+        else:
+            logger.info(f'Not fetching any {model_name.upper()} wind data, probably because it already exists for given date range ({start_time} -- {end_time})')
+    else:
+        pass
+    # Save wind data.
+    if plot_types_config['wind']:
+        if run_query:
+            try:
+                model_data['wind_speed'] = np.sqrt(model_data['u10']**2 + model_data['v10']**2)
+                save_cols = model_config['wind_var_list'] + ['wind_speed']
+            except:
+                logger.warning('Unable to calculate wind speed from u10 and v10 variables.')
+                import pdb; pdb.set_trace()
+                save_cols = model_config['wind_var_list']
+            write_output.df_sealens_parquet(
+                model_data,
+                data_dir / f'wind/{forecast_type_dir}/{model_name}',
+                column_names=save_cols,
+                append=append_data
+            )
+    # Save pressure data.
+    if plot_types_config['pressure']:
+        if run_query:
+            try:
+                model_data['ps'] = model_data['ps'] / 100.0
+            except:
+                logger.warning('Unable to convert pressure to hPa.')
+            write_output.df_sealens_parquet(
+                model_data,
+                data_dir / f'pressure/{forecast_type_dir}/{model_name}',
+                column_names=model_config['pressure_var_list'],
+                append=append_data
+            )
         
--- stofs-event-dashboard/process-event-data.py
+++ stofs-event-dashboard/process-event-data.py
@@ -1,8 +1,10 @@
-"""Process data for example dashboard using Hurricane Milton (2024).
+"""Process data for dashboard.
 
-Contains only the function "main".
-Run from the command line without any additional arguments:
-$ python milton_example.py
+Run from the command line:
+    $ python process-event-data.py <config_path>
+
+E.g., 
+    $ python process-event-data.py ../test_2025.conf
 
 """
 
@@ -11,22 +13,20 @@ import argparse
 import json
 import sys
 import logging
-#
-import space_time_bounds
 import shapely
 import searvey
-from station_obs import fetch_coops_multistation_df
-from models import (
-    get_forecast_init_times, 
-    get_forcing_model, 
-    get_forcing_geometry
-)
-import static_map
 import pandas as pd
 import numpy as np
 import pathlib
-from write_parquet import df_to_sealens
 from seanode.api import get_surge_model_at_stations
+import space_time_bounds
+from station_obs import save_obs
+from models import (
+    get_forecast_init_times, 
+    save_model
+)
+import static_map
+from write_output import df_sealens_parquet
 
 
 logging.basicConfig(level=logging.INFO)
@@ -39,87 +39,46 @@ def process_event(config: dict) -> None:
     """
     # Get space and time bounds.
     stb = space_time_bounds.eventSpaceTimeBounds(config['event'])
-    
-    # Define output directory. 
-    if config['output_dir']:
-        data_dir = pathlib.Path(config['output_dir'])
-    else:
-        data_dir = pathlib.Path(f'../data/{'_'.join([stb.name, str(stb.year)])}')
 
-    # Get station list for event region. 
+    # Get station lists for event region, and format as needed. 
     station_list = searvey.get_coops_stations(metadata_source='main')
     station_list = searvey.get_coops_stations(
         region=stb.get_region(config['stations']['bounds'])\
                   .buffer(config['stations']['buffer']),  
         metadata_source='main'
     )
+    station_list= station_list.reset_index().rename(
+        columns={'lat':'latitude', 'lon':'longitude'}
+    )
+    station_list['station'] = station_list['nos_id']
+    waterlevel_stations = station_list[
+        (station_list['status'] == 'active') & 
+        (station_list['station_type'] == 'waterlevels')
+    ]
+    met_stations = station_list[
+        (station_list['status'] == 'active') & 
+        (station_list['station_type'] == 'met')
+    ]
     
     # ---------- Observations. ------------------------------
     if config['plot_types']['water_level']:
-        waterlevel_stations = station_list[
-            (station_list['status'] == 'active') & 
-            (station_list['station_type'] == 'waterlevels')
-        ]
-        logger.info(f'Fetching {len(waterlevel_stations.index)} stations for water level data.')
-        waterlevel_obs = fetch_coops_multistation_df(
-            waterlevel_stations.index,
-            stb.start_datetime,
-            stb.end_datetime,
-            'water_level',
-            datum=config['output_datum'] 
-        )
-        df_to_sealens(
-            waterlevel_obs,
-            data_dir / 'cwl/obs',
-            column_names=['value']
-        )
-    if config['plot_types']['pressure'] | config['plot_types']['wind']:
-        met_stations = station_list[
-            (station_list['status'] == 'active') & 
-            (station_list['station_type'] == 'met')
-        ]
+        save_obs(waterlevel_stations, stb, 'water_level', config['output'])
     if config['plot_types']['pressure']:
-        logger.info(f'Fetching {len(met_stations.index)} stations for pressure data.')
-        pressure_obs = fetch_coops_multistation_df(
-            met_stations.index,
-            stb.start_datetime,
-            stb.end_datetime,
-            'air_pressure'
-        ) 
-        df_to_sealens(
-            pressure_obs,
-            data_dir / 'pressure/obs',
-            column_names=['value']
-        )
+        save_obs(met_stations, stb, 'pressure', config['output'])
     if config['plot_types']['wind']:
-        logger.info(f'Fetching {len(met_stations.index)} stations for wind data.')
-        wind_obs = fetch_coops_multistation_df(
-            met_stations.index,
-            stb.start_datetime,
-            stb.end_datetime,
-            'wind' 
-        ) 
-        # Calculate u and v components.
-        # u = speed * cos(270 - bearing_from)
-        # v = speed * sin(270 - bearing_from)
-        wind_obs['u_wind'] = \
-            wind_obs['speed'] * np.cos(np.radians(270.0 - wind_obs['degree']))
-        wind_obs['v_wind'] = \
-            wind_obs['speed'] * np.sin(np.radians(270.0 - wind_obs['degree']))
-        wind_obs = wind_obs.rename(columns={'speed':'wind_speed'})
-        df_to_sealens(
-            wind_obs,
-            data_dir / 'wind/obs',
-            column_names=['wind_speed', 'degree', 'direction', 'gust', 
-                          'u_wind', 'v_wind']
-        )
+        save_obs(met_stations, stb, 'wind', config['output'])
 
     # ---------- Model data. ------------------------------
-    # Datum name.
-    if config['output_datum'] == 'NAVD':
-        output_datum_model = 'NAVD88'
-    #
+    # Loop over models.
     for model in config['models'].keys():
+        
+        if config['forecast_type']['nowcast']:
+            logger.info(f'Saving {model} nowcast data.')
+            save_model(waterlevel_stations, met_stations,
+                       stb, {model:config['models'][model]},
+                       config['plot_types'], config['output'],
+                       stb.start_datetime, stb.end_datetime)
+            
         # Get the forecast times for this model.
         # This is reused across variables.
         if config['forecast_type']['all_forecasts']:
@@ -130,163 +89,13 @@ def process_event(config: dict) -> None:
             forecast_inits = config['models'][model]['forecast_init_list']
         logger.info(f'{model} forecast initializations: ')
         logger.info(f'{forecast_inits}')
-        # 
-        if config['plot_types']['water_level']:
-            if config['forecast_type']['nowcast']:
-                model_data = get_surge_model_at_stations(
-                    model.upper(),
-                    config['models'][model]['water_level_var_list'],
-                    waterlevel_stations.index,
-                    stb.start_datetime,
-                    stb.end_datetime,
-                    'nowcast',
-                    'points',
-                    output_datum_model,
-                    'AWS'
-                )
-                df_to_sealens(
-                    model_data,
-                    data_dir / f'cwl/nowcast/{model}',
-                    column_names=config['models'][model]['water_level_var_list']
-                )
-            if forecast_inits:
-                for fidt in forecast_inits:
-                    model_data = get_surge_model_at_stations(
-                        model.upper(),
-                        config['models'][model]['water_level_var_list'],
-                        waterlevel_stations.index,
-                        fidt,
-                        None,
-                        'forecast',
-                        'points',
-                        output_datum_model,
-                        'AWS'
-                    )
-                    df_to_sealens(
-                        model_data,
-                        data_dir / f'cwl/forecast_{fidt.strftime('%Y%m%dT%H%M')}/{model}',
-                        column_names=config['models'][model]['water_level_var_list']
-                    )
-        if config['forecast_type']['nowcast']:
-            if config['plot_types']['pressure'] & config['plot_types']['wind']:
-                # Combine wind and pressure data queries if both are needed.
-                model_data = get_surge_model_at_stations(
-                    get_forcing_model(model),
-                    config['models'][model]['pressure_var_list'] + 
-                    config['models'][model]['wind_var_list'],
-                    met_stations.index,
-                    stb.start_datetime,
-                    stb.end_datetime,
-                    'nowcast',
-                    get_forcing_geometry(model),
-                    None,
-                    'AWS'
-                )
-            elif config['plot_types']['pressure']:
-                model_data = get_surge_model_at_stations(
-                    get_forcing_model(model),
-                    config['models'][model]['pressure_var_list'],
-                    met_stations.index,
-                    stb.start_datetime,
-                    stb.end_datetime,
-                    'nowcast',
-                    get_forcing_geometry(model),
-                    None,
-                    'AWS'
-                )
-            elif config['plot_types']['wind']:
-                model_data = get_surge_model_at_stations(
-                    get_forcing_model(model),
-                    config['models'][model]['wind_var_list'],
-                    met_stations.index,
-                    stb.start_datetime,
-                    stb.end_datetime,
-                    'nowcast',
-                    get_forcing_geometry(model),
-                    None,
-                    'AWS'
-                )
-            else:
-                pass
-            if config['plot_types']['wind']:
-                try:
-                    model_data['wind_speed'] = np.sqrt(model_data['u10']**2 + model_data['v10']**2)
-                    save_cols = config['models'][model]['wind_var_list'] + ['wind_speed']
-                except:
-                    logger.warning('Unable to calculate wind speed from u10 and v10 variables.')
-                    save_cols = config['models'][model]['wind_var_list']
-                df_to_sealens(
-                    model_data,
-                    data_dir / f'wind/nowcast/{model}',
-                    column_names=save_cols
-                )
-            if config['plot_types']['pressure']:
-                df_to_sealens(
-                    model_data,
-                    data_dir / f'pressure/nowcast/{model}',
-                    column_names=config['models'][model]['pressure_var_list']
-                )
-        #
         if forecast_inits:
             for fidt in forecast_inits:
-                if config['plot_types']['pressure'] & config['plot_types']['wind']:
-                    # Combine wind and pressure data queries if both are needed.
-                    model_data = get_surge_model_at_stations(
-                        get_forcing_model(model),
-                        config['models'][model]['pressure_var_list'] + 
-                        config['models'][model]['wind_var_list'],
-                        met_stations.index,
-                        fidt,
-                        None,
-                        'forecast',
-                        get_forcing_geometry(model),
-                        None,
-                        'AWS'
-                    )
-                elif config['plot_types']['pressure']:
-                    model_data = get_surge_model_at_stations(
-                        get_forcing_model(model),
-                        config['models'][model]['pressure_var_list'],
-                        met_stations.index,
-                        fidt,
-                        None,
-                        'forecast',
-                        get_forcing_geometry(model),
-                        None,
-                        'AWS'
-                    )
-                elif config['plot_types']['wind']:
-                    model_data = get_surge_model_at_stations(
-                        get_forcing_model(model),
-                        config['models'][model]['wind_var_list'],
-                        met_stations.index,
-                        fidt,
-                        None,
-                        'forecast',
-                        get_forcing_geometry(model),
-                        None,
-                        'AWS'
-                    )
-                else:
-                    pass
-                if config['plot_types']['pressure']:
-                    df_to_sealens(
-                        model_data,
-                        data_dir / f'pressure/forecast_{fidt.strftime('%Y%m%dT%H%M')}/{model}',
-                        column_names=config['models'][model]['pressure_var_list']
-                    )
-                if config['plot_types']['wind']:
-                    try:
-                        model_data['wind_speed'] = np.sqrt(model_data['u10']**2 + model_data['v10']**2)
-                        save_cols = config['models'][model]['wind_var_list'] + ['wind_speed']
-                    except:
-                        logger.warning('Unable to calculate wind speed from u10 and v10 variables.')
-                        save_cols = config['models'][model]['wind_var_list']
-                    df_to_sealens(
-                        model_data,
-                        data_dir / f'wind/forecast_{fidt.strftime('%Y%m%dT%H%M')}/{model}',
-                        column_names=save_cols
-                    )
+                logger.info(f'Saving {model} forecast data for {fidt.strftime('%Y%m%dT%H%M')}.')
+                save_model(waterlevel_stations, met_stations,
+                           stb, {model:config['models'][model]},
+                           config['plot_types'], config['output'],
+                           fidt, None)
     #
     return stb
         
